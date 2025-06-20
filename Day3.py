012d0c4287d6b1a8fbe3703873c824cfce5b652c
Siri-Pulakanti <pulakantisiri1@gmail.com>
2025-06-20 21:46:35 +0530

    Day3

---
+++ Day3.py
@@ -0,0 +1,16 @@
+def boxOutlinePattern(m,n):
+    for i in range(m):
+        for j in range(n):
+            if(i==0 or i==m-1):
+                
+                print("*",end=" ")
+                
+            elif(j==0 or j==n-1):
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+        print(" ")
+            
+        
+   
+boxOutlinePattern(4,4) 
