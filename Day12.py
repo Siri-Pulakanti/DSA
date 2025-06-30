5d6c639b0e8f1511dbcb58c46a8f51b88863f403
Siri-Pulakanti <pulakantisiri1@gmail.com>
2025-06-30 21:42:21 +0530

    Day12

--- Day12.py
+++ Day12.py
@@ -1,1 +1,13 @@
 https://leetcode.com/problems/contains-duplicate-ii/description/
+
+https://leetcode.com/problems/maximum-average-subarray-i/
+
+https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+https://leetcode.com/problems/fibonacci-number/description/
+
+https://leetcode.com/problems/power-of-two/description/
+
+https://leetcode.com/problems/single-number/
+
+https://leetcode.com/problems/missing-number/description/
