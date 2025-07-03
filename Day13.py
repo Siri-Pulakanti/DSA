edd29d14ed2dceda5326a699e8ed0d13266d6b66
Siri-Pulakanti <pulakantisiri1@gmail.com>
2025-07-03 06:21:36 +0530

    Day13

---
+++ Day13.py
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/running-sum-of-1d-array/description/
+
+https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/
+
+https://leetcode.com/problems/alternating-digit-sum/description/
+
+https://leetcode.com/problems/add-digits/description/
+
+https://leetcode.com/problems/rotate-array/submissions/
+
+https://leetcode.com/problems/clumsy-factorial/description/
+
+https://leetcode.com/problems/get-maximum-in-generated-array/description/
